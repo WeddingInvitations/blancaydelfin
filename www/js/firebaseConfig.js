33f4776bcd1746e2af9e0b43be99cb6951f207b4
O001830 <fernando.aguilar.santos@bbva.com>
2025-01-19 19:27:34 +0100

    init

---
+++ www/js/firebaseConfig.js
@@ -0,0 +1,17 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+// import { enableLogging } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIGAgwzGHhOSGyRVa-Ti6WxGSs_Qo2YWg",
+  authDomain: "keen-oasis-445915-f2.firebaseapp.com",
+  projectId: "keen-oasis-445915-f2",
+  storageBucket: "keen-oasis-445915-f2.firebasestorage.app",
+  messagingSenderId: "423457410411",
+  appId: "1:423457410411:web:6bb1ecb7565834455d502e",
+  measurementId: "G-0JLMQTBNBT"
+};
+
+
+export const firebaseApp = initializeApp(firebaseConfig);
+// enableLogging(true);
